1b3ee7f280d8a01d134c59738b2788a7354f0b04
Shah <shvaisha@amazon.com>
2017-11-03 22:44:35 +0530

    github pages readme removed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import './App.scss';
+import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        Hey there! Welcome.
-      </div>
-    );
+    return <div className="App">Hey there! Welcome.</div>;
   }
 }
 
